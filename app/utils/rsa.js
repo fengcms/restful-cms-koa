50d4cb0080294ca5aba5b67a58d693b7ab953d78
FungLeo <web@fengcms.com>
2020-04-23 16:26:29 +0800

    优化 rsa 秘钥文件路径错误的错误处理

--- app/utils/rsa.js
+++ app/utils/rsa.js
@@ -1,20 +1,24 @@
 const fs = require('fs')
 const NodeRSA = require('node-rsa')
 const { RSA_PRIVATE_KEY_PATH, RSA_PUBLIC_KEY_PATH } = require(':config').KEY
+
+// 公钥加密方法
 const encrypt = (str) => {
   return new Promise((resolve, reject) => {
     fs.readFile(RSA_PUBLIC_KEY_PATH, (err, data) => {
-      if (err) return new Error(err)
+      if (err) reject(new Error(err))
+
       const Rsa = new NodeRSA(data)
       resolve(Rsa.encrypt(str, 'base64'))
     })
   })
 }
-
+// 私钥解密方法
 const decrypt = (str) => {
   return new Promise((resolve, reject) => {
     fs.readFile(RSA_PRIVATE_KEY_PATH, (err, data) => {
-      if (err) return new Error(err)
+      if (err) reject(new Error(err))
+
       const Rsa = new NodeRSA(data)
       try {
         const res = Rsa.decrypt(str, 'utf-8')
